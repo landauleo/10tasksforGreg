9572d813c4929dd2502b9fc46600d66fe75e5c79
landauleo <bambie7@mail.ru>
2019-02-09 20:53:56 +0300

    final fix(2)

--- src/test/java/task2Test.java
+++ src/test/java/task2Test.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.*;
 public class Task2Test {
 
     @Test
-    public void getSumTest() {
-        assertEquals(6.8, Task2.getSum(5), 0.1);
+    public void getFunctionTest() {
+        assertEquals(6.8, Task2.getFunction(5), 0.1);
     }
 }
