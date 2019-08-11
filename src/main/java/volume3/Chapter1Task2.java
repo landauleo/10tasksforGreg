944ce7c07fe1458a4c437392f29c1d73c89a3660
ruanaaf <bambie7@mail.ru>
2019-08-11 18:01:11 +0300

    (Cracking the coding interview) 1.2

--- src/main/java/volume3/Chapter1Task2.java
+++ src/main/java/volume3/Chapter1Task2.java
@@ -4,10 +4,11 @@ import java.util.Arrays;
 
 public class Chapter1Task2 {
     public static void main(String[] args) {
-        System.out.println("One string is a permutation of another: " + isStringPermutation("твойкодговно", "говнотвойкод"));
+        System.out.println("One string is a permutation of another: " + isStringPermutationV2("твойкодговно", "говнотвойкод"));
     }
 
-    public static boolean isStringPermutation(String str1, String str2) {
+
+    static boolean isStringPermutationV1(String str1, String str2) {
         char[] sortedStr1 = str1.toCharArray();
         char[] sortedStr2 = str2.toCharArray();
         Arrays.sort(sortedStr1);
@@ -17,4 +18,19 @@ public class Chapter1Task2 {
 
     }
 
+    public static boolean isStringPermutationV2(String str1, String str2) {
+        char[] sortedStr1 = str1.toCharArray();
+        char[] sortedStr2 = str2.toCharArray();
+
+        return Arrays.equals(customSort(sortedStr1), customSort(sortedStr2));
+
+    }
+
+    public static char[] customSort (char[] arr) {
+        Arrays.sort(arr);
+        return arr;
+    }
 }
+
+
+
--- src/test/java/volume3/Chapter1Task2Test.java
+++ src/test/java/volume3/Chapter1Task2Test.java
@@ -3,12 +3,12 @@ package volume3;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
-import static volume3.Chapter1Task2.isStringPermutation;
+import static volume3.Chapter1Task2.isStringPermutationV1;
 
 public class Chapter1Task2Test {
 
     @Test
     public void getPermutation() {
-        assertTrue(isStringPermutation("говно", "гооно"));
+        assertTrue(isStringPermutationV1("говно", "гооно"));
     }
 }
