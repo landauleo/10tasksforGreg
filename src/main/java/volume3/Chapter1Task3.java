95337d1493d4cb2ec79aaf1c883af1e4429f311f
ruanaaf <bambie7@mail.ru>
2019-08-12 23:58:05 +0300

    (Cracking the coding interview) 1.3 (rework)

--- src/main/java/volume3/Chapter1Task3.java
+++ src/main/java/volume3/Chapter1Task3.java
@@ -5,11 +5,25 @@ public class Chapter1Task3 {
     static char[] arr = {'G', 'r', 'e', ' ', ' ', 'g'};
 
     public static void main(String[] args) {
-        System.out.println("Array after replacing: " + replaceSpaces(arr));
+        System.out.println("Array after replacing: " + replaceSpacesV1(arr));
     }
 
-    public static String replaceSpaces(char[] arr) {
+    public static String replaceSpacesV1(char[] arr) {
         String str = new String(arr);
         return str.replaceAll("\\s", "%20");
     }
+
+    public static String replaceSpacesV2(char[] arr) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == ' ') {
+                stringBuilder.append("%20");
+            }
+            else {
+                stringBuilder.append(arr[i]);
+            }
+        }
+        return stringBuilder.toString();
+    }
+
 }
